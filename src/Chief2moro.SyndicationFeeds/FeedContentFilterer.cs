bd33552c15e3e633dce0b974ce7511b47d432f9d
Mark Everard <markeverard@hotmail.com>
2015-08-20 00:25:27 +0100

    investigate filter access and fix image

--- src/Chief2moro.SyndicationFeeds/FeedContentFilterer.cs
+++ src/Chief2moro.SyndicationFeeds/FeedContentFilterer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Chief2moro.SyndicationFeeds.Models;
 using EPiServer.Core;
@@ -10,13 +11,9 @@ namespace Chief2moro.SyndicationFeeds
     {
         public IEnumerable<IContent> FilterSyndicationContent(IEnumerable<IContent> syndicationContentItems, SyndicationFeedPageType feedPage)
         {
-            //filter by publish rights and access
-            var filteredItems = FilterForVisitor.Filter(syndicationContentItems);
-
             //filter editor set excluded types
             var excludedAllTypes = ParseExcludedIds(feedPage.ExcludedContentTypes);
-          
-            filteredItems = filteredItems.Where(c => !excludedAllTypes.Contains(c.ContentTypeID));
+            var filteredItems = syndicationContentItems.Where(c => !excludedAllTypes.Contains(c.ContentTypeID)).ToList();
 
             //filter by category
             if (feedPage.CategoryFilter == null)
@@ -26,9 +23,14 @@ namespace Chief2moro.SyndicationFeeds
             {
                 filteredItems = filteredItems
                     .Where(c => c is ICategorizable)
-                    .Where(c => ((ICategorizable)c).Category.MemberOfAny(feedPage.CategoryFilter));
+                    .Where(c => ((ICategorizable)c).Category.MemberOfAny(feedPage.CategoryFilter)).ToList();
             }
 
+            //block types are alkways removed by filter for visitor. We want to see them and respect access rights
+            var blockTypes = filteredItems.Where(c => c is BlockData);
+            new FilterContentForVisitor().Filter(filteredItems);
+            filteredItems.AddRange(blockTypes);
+            
             return filteredItems;
         }
 
